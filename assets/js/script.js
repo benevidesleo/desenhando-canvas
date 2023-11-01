dedccdcf519ab9d3610c50bfb2e09440444c06b8
Leonardo Luis <leoluisbsantos16@gmail.com>
2023-11-01 01:02:59 -0300

    projeto finalizado e corrigido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,68 @@
 
 // INITIAL DATA
 let currentColor = 'black'
-let scren = document.querySelector('#tela')
-let context = scren.getContext('2d')
+let screen = document.querySelector('#tela')
+let context = screen.getContext('2d')
 let draw = false
 let mouseX = 0
 let mouseY = 0
 
 
 // EVENTS
-document.querySelectorAll('.colorArea .color').forEach((item)=> { 
-    item.addEventListener('click', colorClick) 
+document.querySelectorAll('.colorArea .color').forEach((item) => {
+    item.addEventListener('click', colorClick)
 })
 
-scren.addEventListener('mousedown', mouseDownE)
-scren.addEventListener('mousemove', mouseMoveE)
-scren.addEventListener('mouseup', mouseUpE)
+screen.addEventListener('mousedown', mouseDownE)
+screen.addEventListener('mousemove', mouseMoveE)
+screen.addEventListener('mouseup', mouseUpE)
+document.querySelector('.clear').addEventListener('click', buttonClear)
 
 
 // FUNCTIONS
-function colorClick(e) { 
+function colorClick(e) {
     let color = e.target.getAttribute('data-color')
     currentColor = color
 
     document.querySelector('.color.active').classList.remove('active')
     e.target.classList.add('active')
 }
-function mouseDownE(e) { 
-   draw = true
-   mouseX = e.pageX - scren.offsetLeft
-   mouseY = e.pageY - scren.offsetTop
+function mouseDownE(e) {
+    draw = true
+    mouseX = e.pageX - screen.offsetLeft
+    mouseY = e.pageY - screen.offsetTop
 }
 
-function mouseMoveE(e) { 
-   if(draw) { 
-   canDraw(e.pageX, e.pageY)
-   } 
+function mouseMoveE(e) {
+    if (draw) {
+        canDraw(e.pageX, e.pageY)
+    }
 }
 
-function mouseUpE() { 
-   draw = false
+function mouseUpE() {
+    draw = false
 }
 
-function canDraw(x, y) { 
-    let pointX = x - scren.offsetLeft
-    let pointY = y - scren.offsetTop
-
+function canDraw(x, y) {
+    let pointX = x - screen.offsetLeft
+    let pointY = y - screen.offsetTop
 
     context.beginPath()
     context.lineWidth = 4
-    context.lineJoin = 'round'
+    context.lineJoin = "round"
     context.moveTo(mouseX, mouseY)
-    context.lineTo(mouseX, mouseY)
-
-
-
-
-
+    context.lineTo(pointX, pointY)
+    context.closePath()
+    context.strokeStyle = currentColor
+    context.stroke()
 
     mouseX = pointX
     mouseY = pointY
+}
+
+function buttonClear() { 
+    context.setTransform(1, 0, 0, 1, 0, 0)
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height)
 }
 
 
