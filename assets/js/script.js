63e4e0fe2696ddbea8b52b75a3e98adb7125038e
Leonardo Luis <leoluisbsantos16@gmail.com>
2023-11-01 00:50:12 -0300

    evento do desenho feito!

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ let currentColor = 'black'
 let scren = document.querySelector('#tela')
 let context = scren.getContext('2d')
 let draw = false
+let mouseX = 0
+let mouseY = 0
 
 
 // EVENTS
@@ -11,9 +13,9 @@ document.querySelectorAll('.colorArea .color').forEach((item)=> {
     item.addEventListener('click', colorClick) 
 })
 
-scren.addEventListener('mousedown', mouseDown)
-scren.addEventListener('mousemove', mouseMove)
-scren.addEventListener('mouseup', mouseUp)
+scren.addEventListener('mousedown', mouseDownE)
+scren.addEventListener('mousemove', mouseMoveE)
+scren.addEventListener('mouseup', mouseUpE)
 
 
 // FUNCTIONS
@@ -24,18 +26,40 @@ function colorClick(e) {
     document.querySelector('.color.active').classList.remove('active')
     e.target.classList.add('active')
 }
-function mouseDown() { 
+function mouseDownE(e) { 
    draw = true
+   mouseX = e.pageX - scren.offsetLeft
+   mouseY = e.pageY - scren.offsetTop
 }
 
-function mouseMove() { 
+function mouseMoveE(e) { 
    if(draw) { 
-     console.log( 'desenho ativo')
+   canDraw(e.pageX, e.pageY)
    } 
 }
 
-function mouseUp() { 
+function mouseUpE() { 
    draw = false
+}
+
+function canDraw(x, y) { 
+    let pointX = x - scren.offsetLeft
+    let pointY = y - scren.offsetTop
+
+
+    context.beginPath()
+    context.lineWidth = 4
+    context.lineJoin = 'round'
+    context.moveTo(mouseX, mouseY)
+    context.lineTo(mouseX, mouseY)
+
+
+
+
+
+
+    mouseX = pointX
+    mouseY = pointY
 }
 
 
